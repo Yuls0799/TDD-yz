d4a92f078faae5a65b8235db3d9021fa16bb704f
Yuliana Zorrilla <107286036+Yuls0799@users.noreply.github.com>
2023-12-11 12:43:49 -0400

    Pass

--- validatePassword.js
+++ validatePassword.js
@@ -1,7 +1,12 @@
 function validatePassword(password) {
-    const validLength = password.length = 8
-    let hasLetter = /[a-zA-Z]/g.test(password)
-    let hasNumber = /[0-9]/g.test(password)
-    return hasNumber && hasLetter && validLength
+    const validLength = password.length >= 8;
+    const hasLetter = /[a-zA-Z]/g.test(password);
+    const hasNumber = /[0-9]/g.test(password);
+
+    if (validLength && hasLetter && hasNumber) {
+        return true;
+    } else {
+        return false;
+    }
 }
 module.exports = validatePassword
